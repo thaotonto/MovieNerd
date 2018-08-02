68b121b6667fcaaf8350cdb908e13cb572793afa
ducbet <ducbet1996@gmail.com>
2018-08-02 15:20:49 +0700

    Show user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,14 @@ class ApplicationController < ActionController::Base
   def default_url_options
     {locale: I18n.locale}
   end
+
+  private
+
+  def find_user
+    @user = User.find_by id: params[:id]
+
+    return if @user
+    flash[:danger] = t "flash.no_user"
+    redirect_to root_url
+  end
 end
--- app/models/screening.rb
+++ app/models/screening.rb
@@ -2,4 +2,5 @@ class Screening < ApplicationRecord
   belongs_to :movie
   belongs_to :room
   has_many :orders
+  has_many :users, through: :orders
 end
