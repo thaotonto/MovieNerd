2bfae6f4b754c8b4cb56b80fb2eff61ffc38a318
ducbet <ducbet1996@gmail.com>
2018-08-02 15:20:49 +0700

    Edit user profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
-user1 = User.create name: "TMD",
-                    email: "a@a.a",
-                    user_type: 0
+user1 = User.create! name: "TMD",
+                     email: "a@a.a",
+                     password: "asdasd",
+                     user_type: 0
 
-user2 = User.create name: "Trieu Minh Duc",
-                    email: "b@b.b",
-                    user_type: 0
+user2 = User.create! name: "Trieu Minh Duc",
+                     email: "b@b.b",
+                     password: "asdasd",
+                     user_type: 0
 
-movie = Movie.create title: "Wall-E",
-                     duration: 115
+movie = Movie.create! title: "Wall-E",
+                      duration: 115
 
-room = Room.create name: "G1",
-                   seat_no: 50
+room = Room.create! name: "G1",
+                    seat_no: 50
 
 seat = room.seats.create!
 
@@ -19,4 +21,3 @@ screening = movie.screenings.create! room_id: room.id
 order = user1.orders.create! screening_id: screening.id
 
 ticket = order.movie_tickets.create! seat_id: seat.id
-
