8f6c3f8f9af4bb81253555029634318e4f98211f
noobsi <quocyoghurt@gmail.com>
2018-08-03 16:48:57 +0700

    SignUp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
   before_action :find_user, only: [:show]
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "flash.welcome"
+      redirect_to root_path
+    else
+      flash[:danger] = t "flash.create_failed"
+      render :new
+    end
+  end
+
   def show; end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
