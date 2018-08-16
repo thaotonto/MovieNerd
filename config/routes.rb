7343e1f1648d805ddf6266283f485d6ee080d0f3
noobsi <quocyoghurt@gmail.com>
2018-08-16 11:50:14 +0700

    about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
+    get "/price", to: "static_pages#price"
+    get "/about", to: "static_pages#about"
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
     get "sessions/new"
